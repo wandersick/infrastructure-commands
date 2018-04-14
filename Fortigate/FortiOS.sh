9c6fa1fde2f8d9c2a53bd907735b6fee53856401
wandersick <wandersick@gmail.com>
2018-04-14 18:17:22 +0800

    Create FortiOS.sh

---
+++ Fortigate/FortiOS.sh
@@ -0,0 +1,10 @@
+get hardware status
+get system status
+get system ha status
+get system interface physical
+get router info routing-table all
+get router info ospf neighbor
+get router info ospf database brief
+get router info ospf database router lsa 
+get system arp
+get system arp-table
