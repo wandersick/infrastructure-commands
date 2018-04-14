946060b9129476e805d679870a8cc0af6c82e8b8
wandersick <wandersick@gmail.com>
2018-04-14 18:16:53 +0800

    Create PAN-OS.sh

---
+++ PaloAlto/PAN-OS.sh
@@ -0,0 +1,23 @@
+set cli pager off
+show system info
+show config running
+set cli config-output-format set
+configure
+show
+exit
+set cli config-output-format xml
+configure
+show
+exit
+show high-availability all
+show routing summary
+show routing route
+show interface all
+show arp all
+show mac all
+show routing protocol ospf neighbour
+show routing protocol ospf summary
+show routing protocol ospf interface
+show routing protocol ospf lsdb
+show routing protocol ospf dumplsdb
+set cli config-output-format default
