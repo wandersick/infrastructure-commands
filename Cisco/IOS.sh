0768bd3e9ae25b21cbe62ab7b1f6c89d91d39478
wandersick <wandersick@gmail.com>
2018-04-14 18:14:46 +0800

    Create IOS.sh

---
+++ Cisco/IOS.sh
@@ -0,0 +1,25 @@
+terminal length 0
+show run
+show version
+show inv
+show env all
+show log 
+show cdp neighbor
+show ip route
+show spanning-tree
+show spanning-tree mst
+show ip interface brief
+show etherchannel summary
+show proc cpu history
+show proc cpu sorted
+show proc memory sorted
+show interface
+show interfaces trunk
+show vtp status
+show ip ospf
+show ip ospf database
+show ip ospf interface
+show vrrp
+show standby
+show switch
+show mac address
