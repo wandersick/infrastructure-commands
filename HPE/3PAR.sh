ca747edd179903654d28b50f1e46d367dd55e4cb
wandersick <wandersick@gmail.com>
2018-04-14 17:59:40 +0800

    Create 3PAR.sh

---
+++ HPE/3PAR.sh
@@ -0,0 +1,108 @@
+showld -d
+showld -state -sortcol 3
+showalert -n
+showsys -d
+showpd -failed -degraded
+showpdch -fail
+showeventlog -min 20
+statport -iter 5
+showalert
+showaocfg
+showauthparam
+showbattery
+showblock
+showcage
+showcert
+showcim
+showclienv
+showcpg
+showdate
+showdomain
+showdomainset
+showeeprom
+showencryption
+showeventlog
+showfirmwaredb
+showflashcache
+showfpg
+showfs
+showfsav
+showfsgroup
+showfshare
+showfsip
+showfsnap
+showfsnapclean
+showfsndmp
+showfsquota
+showfsroute
+showfstore
+showfsuser
+showhost
+showhost -pathsum
+showhostset
+showinventory
+showiscsisession
+showld
+showldch
+showldmap
+showlicense
+shownet
+shownode
+shownodeenv
+showpatch
+showpd
+showpdata
+showpdch
+showpdvv
+showport
+showportarp
+showportdev
+showportisns
+showportlesb
+showportpel
+showqos
+showrcopy
+showrctransport
+showrole
+showrsv
+showsched
+showsnmpmgr
+showsnmppw
+showsnmpuser
+showspace
+showspare
+showsr
+showsralertcrit
+showsshbanner
+showsshkey
+showsys
+showsys -param
+showsysmgr
+showtarget
+showtask
+showtemplate
+showtoc
+showtocgen
+showuser
+showuseracl
+showuserconn
+showvasa
+showversion
+showvfs
+showvlun
+showvlun ao showvlun -v
+showvv
+showvvcpg
+showvvmap
+showvvolsc
+showvvolvm
+showvvpd
+showvvset
+showwsapi
+showwsapisession
+checkhealth
+checkpassword
+checkport
+checkpd
+checkld
+checkvv
