041362f5cb36226aeaff981c3a2195e0e86d3469
wandersick <wandersick@gmail.com>
2018-04-14 18:20:10 +0800

    Create EX_Switch.sh

---
+++ Juniper/EX_Switch.sh
@@ -0,0 +1,9 @@
+show version | no-more
+show chassis hardware | no-more
+show configuration | no-more
+show configuration | no-more | display set
+show interfaces terse | no-more
+show route summary | no-more
+show route | no-more
+show arp | no-more
+show ethernet-switching table | no-more
