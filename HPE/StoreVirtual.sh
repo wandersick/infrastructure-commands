ebfab7bd4fef684a2e263c653027bf61082d54ca
wandersick <wandersick@gmail.com>
2019-01-31 13:27:38 +0800

    Add utilities supported by VSA

--- HPE/StoreVirtual.sh
+++ HPE/StoreVirtual.sh
@@ -35,3 +35,66 @@ utility run="lsof"
 utility run="netstat -a"
 utility run="netstat -tulpen"
 utility run="netstat -tupen"
+
+; For a list of all supported utilities, enter 'utility'
+; 
+; CLIQ>utility
+; 
+; HPE StoreVirtual LeftHand OS Command Line Interface, v12.6.00.0155
+; (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+; 
+; This command is recommended for HPE support only.
+; Are You Sure (y/n)? y
+; 
+; 
+; RESPONSE
+;  result         0
+;  processingTime 151
+;  name           CliqSuccess
+;  description    Operation succeeded.
+; 
+;   UTILITY
+; 
+; arp
+; cat
+; createschedule
+; deleteschedule
+; df
+; dmesg
+; du
+; dumpdb
+; env
+; find
+; forceauth
+; gatherlogs
+; groupsupportinfo
+; haldiag
+; hardwarereport
+; ifconfig
+; iperf
+; iperfstatus
+; ls
+; lsof
+; lspci
+; netigram
+; netstat
+; nmap
+; nsmsupportinfo
+; ntpq
+; perldb
+; ping
+; platformlogs
+; ps
+; pwd
+; recoverquorum
+; rmlog
+; saniqtrace
+; service
+; servicectl
+; tcpdump
+; textdb
+; top
+; tpcqueues
+; traceroute
+; xmldb
+; zcat
