514d1d2c5f6d9e7720c06339b41a3f4a69b8141a
wandersick <wandersick@gmail.com>
2019-03-22 15:13:40 +0800

    Create Comware.sh

---
+++ H3C/Comware.sh
@@ -0,0 +1,19 @@
+screen-length disable 
+display version
+display clock
+display device manuinfo
+display device
+display interface
+display vlan
+display vlan all
+display ip interface
+display ip interface brief
+display ip interface Vlan-interface
+display ip interface Vlan-interface brief
+display ip routing-table
+display ip protocol
+display ip statistics
+display current-configuration
+display saved-configuration
+display base-information 
+display diagnostic-information
