d58dce288654a64870f3f3786e98d113905065ee
wandersick <wandersick@gmail.com>
2019-04-10 13:47:06 +0800

    Create Switch.sh

---
+++ Aruba/Switch.sh
@@ -0,0 +1,43 @@
+# Reference: Aruba 2930F / 2930M Management and Configuration Guide for ArubaOS Switch 16.05
+
+show running-config
+show config
+show ip
+show captive-portal
+show port-access clients
+show radius authentication
+show radius dyn-authorization
+show radius accounting
+show crypto pki local-certificate
+
+show vsf
+show vsf detail
+show vsf link
+show vsf link detail
+show vsf member
+Show vsf topology
+show vsf topologydetail
+show vsf topology change-history
+show vsf lldp-mad
+show vsf vlan-mad
+show vsf trunk-designated-fowvarder
+
+show cpu
+show power-over-ethernet
+show system information
+show system information vsf member
+show system fans
+show system chassislocate
+show system power-supply
+
+show ntp statistics
+show ntp status
+show ntp associations
+show ntp authentication
+
+show ptp
+
+show usb-port
+
+show mac-add detail
+show mac-address
