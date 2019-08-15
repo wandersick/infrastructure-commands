668fb999e545096476b17ef968ea79e7269101fb
wandersick <wandersick@gmail.com>
2019-08-15 15:23:31 +0800

    Create FabricOS-create.sh

---
+++ Brocade/FabricOS-create.sh
@@ -0,0 +1,12 @@
+switchshow
+alicreate, ALIAS_NAME, WWN
+alishow
+zonecreate, ZONE_NAME, "ALIAS_NAME1; ALIAS_NAME2; ..."
+zoneshow
+ipaddrset
+cfgactvshow
+cfgcreate, CFG_NAME, ZONE_NAME1
+cfgadd, CFG_NAME, ZONE_NAME2
+cfgadd, CFG_NAME, ZONE_NAME3
+cfgsave
+cfgenable
