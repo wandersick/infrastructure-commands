bdc9237694ef94ac23e34e604e2b5219e7cee59e
wandersick <wandersick@gmail.com>
2019-10-04 13:02:14 +0800

    Add switchname

--- Brocade/FabricOS-create.sh
+++ Brocade/FabricOS-create.sh
@@ -1,9 +1,10 @@
 switchshow
+switchname
+ipaddrset
 alicreate, ALIAS_NAME, WWN
 alishow
 zonecreate, ZONE_NAME, "ALIAS_NAME1; ALIAS_NAME2; ..."
 zoneshow
-ipaddrset
 cfgactvshow
 cfgcreate, CFG_NAME, ZONE_NAME1
 cfgadd, CFG_NAME, ZONE_NAME2
