611dbc68a4d86ca8c399cdb91246fa3503254023
wandersick <wandersick@gmail.com>
2021-03-19 14:07:21 +0800

    Add HPE StoreOnce commands

---
+++ HPE/StoreOnce_all_commands.sh
@@ -0,0 +1,209 @@
+accounts add
+accounts del
+accounts modify
+accounts show
+accounts unlock
+alerts show
+accounts sync
+cifs add localadmin
+cifs add user
+cifs delete localadmin
+cifs delete user
+cifs disable domainjoin
+cifs disable user
+cifs enable domainjoin
+cifs enable user
+cifs modify config
+cifs modify user
+cifs show config
+cifs show localadmin
+config delete
+config restore devices
+config restore keystore
+config save devices
+config save keystore
+config show list
+exit
+hardware add storage
+hardware beacon
+hardware delete storage
+hardware disable failover
+hardware discover storage
+hardware enable failover
+hardware failback
+hardware failover
+hardware poweroff
+hardware poweron
+hardware prepare storage
+hardware reboot
+hardware set ports
+hardware show firmware
+hardware show net status
+hardware show node status
+hardware show problems
+hardware show status
+hardware show storage status
+hardware update firmware
+help
+keymanager certificate
+keymanager create
+keymanager expand
+keymanager import
+keymanager mode external
+keymanager mode local
+keymanager mode show
+keymanager update
+license add
+license hardware activate
+license hardware show
+license load
+license refresh
+license show all
+license show cloudbank
+license show summary
+logs add recipient
+logs delete recipient
+log modify
+logs show recipient
+logs export events
+logs show events
+logs show update
+nas create share
+nas delete share
+nas modify share
+nas show share
+net activate config
+net activate status
+net add encryption
+net add ipaddr
+net add portset
+net add route
+net add subnet
+net copy config
+net create config
+net create portset
+net delete config
+net delete encryption
+net delete ipaddr
+net delete portset
+net delete route
+net delete subnet
+net modify config
+net modify portset
+net modify subnet
+net ping
+net set address
+net set wizard
+net show config
+net show ipaddr
+net show ports
+net show portset
+net show status
+net validate config
+nfs add host
+nfs delete host
+nfs disable browsing
+nfs disable host
+nfs enable browsing
+nfs enable host
+nfs modify host
+nfs show config
+serviceset show status
+serviceset start
+serviceset stop
+snmp add trapsink
+snmp add user
+snmp del trapsink
+snmp del user
+snmp disable
+snmp enable
+snmp modify trapsink
+snmp modify user
+snmp reset ro-community
+snmp send testtrap
+snmp set contact
+snmp set location
+snmp set ro-community
+snmp show config
+snmp show user
+support create ticket
+support del ticket
+support show ticket
+support warranty show
+system clear banner
+system clear packages
+system clear repository
+system confidencechecker
+system disable accountlockout
+system disable failover
+system disable remoteeventsuppression
+system disable staticciphers
+system disable tls
+system enable accountlockout
+system enable failover
+system enable remoteeventsuppression
+system enable staticciphers
+system enable tls
+system failback
+system failover
+system import certificate
+system load packages
+system move afm
+system reboot
+system recover filesystem
+system restart remotesupport
+system set accountlockout attemptinterval
+system set accountlockout maxattempts
+system set accountlockout lockduration
+system set banner
+system set clioutput
+system set clusterid
+system set config
+system set description
+system set eventaggregation period
+system set pagecontrol
+system set remotesupport customerinfo
+system set remotesupport register
+system set timeout
+system show accountlockout
+system show config
+system show packages
+system show performance
+system show remoteeventsuppression
+system show remotesupport customerinfo
+system show remotesupport register
+system show repository
+system show staticciphers
+system show status
+system show timeout
+system show tls
+system shutdown
+system update packages
+system update remotesupport customerinfo
+system update remotesupport register
+system set remotesupport restore_customerinfo
+system set remotesupport restore_register
+time add ntpserver
+time del ntpserver
+time reset timezone
+time set timezone
+time set UTC
+time show config
+vtl create cartridge
+vtl create library
+vtl delete cartridge
+vtl delete cartridges
+vtl delete library
+vtl erase cartridge
+vtl erase cartridges
+vtl modify cartridge
+vtl modify drive
+vtl modify library
+vtl move cartridge
+vtl show capacity
+vtl show cartridges
+vtl show drive info
+vtl show drive list
+vtl show library info
+vtl show library list
+vtl show ports
---
+++ HPE/StoreOnce_show_commands.sh
@@ -0,0 +1,51 @@
+accounts show
+alerts show
+cifs show config
+cifs show localadmin
+config show list
+hardware show firmware
+hardware show net status
+hardware show node status
+hardware show problems
+hardware show status
+hardware show storage status
+keymanager mode show
+license hardware show
+license show all
+license show cloudbank
+license show summary
+logs show recipient
+logs show events
+logs show update
+nas show share
+net show config
+net show ipaddr
+net show ports
+net show portset
+net show status
+nfs show config
+serviceset show status
+snmp show config
+snmp show user
+support show ticket
+support warranty show
+system show accountlockout
+system show config
+system show packages
+system show performance
+system show remoteeventsuppression
+system show remotesupport customerinfo
+system show remotesupport register
+system show repository
+system show staticciphers
+system show status
+system show timeout
+system show tls
+time show config
+vtl show capacity
+vtl show cartridges
+vtl show drive info
+vtl show drive list
+vtl show library info
+vtl show library list
+vtl show ports
